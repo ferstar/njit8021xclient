8e6f2b4b42085d72842d40f7db48e39db96fad55
ferstar <zhangjianfei3@gmail.com>
2012-11-22 20:37:59 +0800

    更改客户端版本号为 EN V3.60-6208

--- src/auth.c
+++ src/auth.c
@@ -30,7 +30,7 @@ typedef enum {IDENTITY=1, NOTIFICATION=2, MD5=4, AVAILABLE=20} EAP_Type;
 typedef uint8_t EAP_ID;
 const uint8_t BroadcastAddr[6] = {0xff,0xff,0xff,0xff,0xff,0xff}; // 广播MAC地址
 const uint8_t MultcastAddr[6]  = {0x01,0x80,0xc2,0x00,0x00,0x03}; // 多播MAC地址
-const char H3C_VERSION[16]="EN V2.40-0335"; // 华为客户端版本号
+const char H3C_VERSION[16]="EN V3.60-6208"; // 华为客户端版本号
 const char H3C_KEY[]      ="HuaWei3COM1X";  // H3C的固定密钥
 
 // 子函数声明
